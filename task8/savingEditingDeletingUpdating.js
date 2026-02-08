098d828d1d9910b3830da6762427d82082d31f89
Gurpreet Singh <gsingh1@weareeverise.com>
2026-02-09 01:09:47 +0530

    updating dataset

--- task8/savingEditingDeletingUpdating.js
+++ task8/savingEditingDeletingUpdating.js
@@ -1,4 +1,11 @@
-let userIdToEdit="";
+let userIdToEdit = "";
+
+function resetForm() {
+  document.getElementById("username").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("phone").value = "";
+}
+
 function handleFormSubmit(event) {
   event.preventDefault();
   const userDetails = {
@@ -7,66 +14,63 @@ function handleFormSubmit(event) {
     phone: event.target.phone.value,
   };
   console.log("userDetails", userDetails)
-  
-  if(userIdToEdit){
-     axios
-    .put(
-      `https://crudcrud.com/api/1a4a9cc4f8cb497d93205dd977d2e446/todo/${userIdToEdit}`,
-      userDetails
-    )
-    .then((response) => {
-      userIdToEdit="";
-      document.getElementById("username").value = "";
-      document.getElementById("email").value = "";
-      document.getElementById("phone").value = "";
-      getUsers();
-    })
-    .catch((error) => console.log(error));
+
+  if (userIdToEdit) {
+    axios
+      .put(
+        `https://crudcrud.com/api/13fb93b180ff48e495331504ca3031d6/todo/${userIdToEdit}`,
+        userDetails
+      )
+      .then((response) => {
+        userIdToEdit = "";
+        resetForm();
+        getUsers();
+      })
+      .catch((error) => console.log(error));
   } else {
-     axios
-    .post(
-      "https://crudcrud.com/api/1a4a9cc4f8cb497d93205dd977d2e446/todo",
-      userDetails
-    )
-    .then((response) => getUsers())
-    .catch((error) => console.log(error));
+    axios
+      .post(
+        "https://crudcrud.com/api/13fb93b180ff48e495331504ca3031d6/todo",
+        userDetails
+      )
+      .then((response) => getUsers())
+      .catch((error) => console.log(error));
   }
- 
+
 
   // Clearing the input fields
-  document.getElementById("username").value = "";
-  document.getElementById("email").value = "";
-  document.getElementById("phone").value = "";
+  resetForm();
 }
 window.addEventListener("DOMContentLoaded", () => {
- getUsers();
+  getUsers();
 });
 
 function getUsers() {
-  
-  
-   axios
+
+
+  axios
     .get(
-      "https://crudcrud.com/api/1a4a9cc4f8cb497d93205dd977d2e446/todo"
+      "https://crudcrud.com/api/13fb93b180ff48e495331504ca3031d6/todo"
     )
     .then((response) => {
-      const liElements = document.querySelectorAll('li'); 
-      liElements.forEach((li)=>{
-            li.remove();
-      })
+      const userList = document.querySelector("ul");
+      userList.innerHTML = "";
       response.data.forEach((userDetails) => {
         displayUserOnScreen(userDetails);
       });
     })
     .catch((error) => console.log(error));
-  
+
 }
 
-function displayUserOnScreen(userDetails){
-  
+function displayUserOnScreen(userDetails) {
+
   const userId = userDetails["_id"]
   const userItem = document.createElement("li");
   userItem.setAttribute("id", userId);
+  userItem.dataset.username = userDetails.username;
+  userItem.dataset.email = userDetails.email;
+  userItem.dataset.phone = userDetails.phone;
   userItem.appendChild(
     document.createTextNode(
       `${userDetails.username}-${userDetails.email}-${userDetails.phone}`
@@ -87,26 +91,25 @@ function displayUserOnScreen(userDetails){
   deleteBtn.addEventListener("click", function (event) {
     const liElement = event.target.parentElement;
     const userId = liElement.getAttribute('id');
-       axios
+    axios
       .delete(
-        
-        `https://crudcrud.com/api/1a4a9cc4f8cb497d93205dd977d2e446/todo/${userId}`
+
+        `https://crudcrud.com/api/13fb93b180ff48e495331504ca3031d6/todo/${userId}`
       )
       .then(() => {
         getUsers();
       })
       .catch((error) => console.log(error));
   });
-  
+
   editBtn.addEventListener("click", function (event) {
-    const livalue = event.target.parentElement.childNodes[0].textContent.split("-");
-    userIdToEdit = event.target.parentElement.getAttribute("id");
-    const username=document.getElementById("username");
-    const email=document.getElementById("email");
-    const phoneno=document.getElementById("phone");
-    username.value=livalue[0];
-    email.value=livalue[1];
-    phoneno.value=livalue[2];
-   
-})
+    const liElement = event.target.parentElement;
+    userIdToEdit = liElement.getAttribute("id");
+    const username = document.getElementById("username");
+    const email = document.getElementById("email");
+    const phoneno = document.getElementById("phone");
+    username.value = liElement.dataset.username || "";
+    email.value = liElement.dataset.email || "";
+    phoneno.value = liElement.dataset.phone || "";
+  })
 }
